636e4b43aba343ab81b4018830f8ac2915241d58
Sahaja-surnoi <Sahajagarrepally@surnoi.com>
2024-08-13 00:12:10 +0530

    Update master.service.ts

--- src/app/service/master.service.ts
+++ src/app/service/master.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class MasterService {
   
   production:true | undefined
-  apiEndPoint: string = "http://ec2-52-221-235-23.ap-southeast-1.compute.amazonaws.com:8080/api/HospitalAppointment/";
+  apiEndPoint: string = "http://ec2-47-129-43-186.ap-southeast-1.compute.amazonaws.com:8080/api/HospitalAppointment/";
   constructor(private http: HttpClient) { }
 
   createNew(obj:any): Observable<any> {
